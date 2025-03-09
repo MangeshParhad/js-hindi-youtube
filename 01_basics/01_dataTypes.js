55a75861b7c5da7f17cea044192842e2c3abbee3
MangeshParhad <142496878+MangeshParhad@users.noreply.github.com>
2025-03-09 03:59:32 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log(4+4)
+console.log("Mangesh")
+
+let name = "Mangesh"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof "Mangesh");
+console.log(typeof undefined);
+console.log(typeof null);
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 845986
+let accountEmail = "mangesh@google.com"
+var accountPassword = "1507"
+accountCity = "ChhSambhajinagar"
+
+accountEmail = "Parhad@.com"
+accountPassword = "5362"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
